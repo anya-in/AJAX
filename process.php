bf1ffb355b1df807356c6fc339694a56e1ed3f4d
Anya <79497125+anya-in@users.noreply.github.com>
2021-05-06 17:07:33 +0530

    Add files via upload

---
+++ process.php
@@ -0,0 +1,22 @@
+<?php
+echo 'Processing ...';
+// Connect to a database
+$conn = mysqli_connect('localhost', 'root', '123456', 'ajaxtest');
+
+// Check for POST variable
+if(isset($_POST['name'])){
+  $name = mysqli_real_escape_string($conn, $_POST['name']);
+  //echo 'POST: Your name is '. $_POST['name'];
+
+  $query = "INSERT INTO users(name) VALUES('$name')";
+
+  if(mysqli_query($conn, $query)){
+    echo 'User Added...';
+  } else {
+    echo 'ERROR: '. mysqli_error($conn);
+  }
+}
+if(isset($_GET['name'])){
+  echo 'GET: Your name is '.$_GET['name'];
+
+}
